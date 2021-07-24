fc57d2f786ab3ef4b0e99bd6fe4f1dd5820083a2
EAGzzyCSL <eagzzycsl@hotmail.com>
2021-07-25 01:12:47 +0800

    chore(infra): remove redundant lint ignore in useStore

--- packages/site/hooks/useStore.ts
+++ packages/site/hooks/useStore.ts
@@ -2,7 +2,7 @@ import React from 'react'
 
 import store from '@/store'
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-const useStore = () => React.useContext(React.createContext(store)) // eslint-disable-line @typescript-eslint/explicit-function-return-type
+const useStore = (): typeof store =>
+  React.useContext(React.createContext(store))
 
 export default useStore
