3e18d2cd02df87d5a2b1b875b8261e31dd33edb3
EAGzzyCSL <eagzzycsl@hotmail.com>
2022-05-01 18:57:47 +0800

    style(infra): css禁止duplicate-properties

--- packages/stylelint/css.js
+++ packages/stylelint/css.js
@@ -16,5 +16,8 @@ module.exports = {
     ],
     'selector-id-pattern': '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
     'csstree/validator': true,
+    // https://stylelint.io/user-guide/rules/list/declaration-block-no-duplicate-properties/
+    // consecutive-duplicates也不允许
+    'declaration-block-no-duplicate-properties': true,
   },
 }
