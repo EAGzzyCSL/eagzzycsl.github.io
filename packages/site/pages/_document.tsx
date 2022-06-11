a62a31014ec95e1e2b4fc1f8c091e430e58dbc40
EAGzzyCSL <eagzzycsl@hotmail.com>
2022-06-11 17:05:18 +0800

    refactor(infra): remove useless @mui/styles

--- packages/site/pages/_document.tsx
+++ packages/site/pages/_document.tsx
@@ -1,43 +1,8 @@
 import React from 'react'
 
-import { ServerStyleSheets } from '@mui/styles'
-import Document, {
-  DocumentContext,
-  DocumentInitialProps,
-  Html,
-  Head,
-  Main,
-  NextScript,
-} from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class MyDocument extends Document {
-  /**
-   * material-ui针对next.js的SSG优化
-   * 参考：https://material-ui.com/zh/styles/advanced/#next-js
-   */
-  static getInitialProps = async (
-    ctx: DocumentContext,
-  ): Promise<DocumentInitialProps> => {
-    const sheets = new ServerStyleSheets()
-    const originalRenderPage = ctx.renderPage
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        // eslint-disable-next-line react/jsx-props-no-spreading, react/display-name
-        enhanceApp: App => props => sheets.collect(<App {...props} />),
-      })
-
-    const initialProps = await Document.getInitialProps(ctx)
-
-    return {
-      ...initialProps,
-      styles: [
-        ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement(),
-      ],
-    }
-  }
-
   render(): JSX.Element {
     return (
       <Html data-greet='Welcome to inspect!'>
