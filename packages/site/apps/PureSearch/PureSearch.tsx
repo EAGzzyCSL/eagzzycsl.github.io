d2ca4922061fed167856b39339e8b77a016d899b
EAGzzyCSL <eagzzycsl@hotmail.com>
2023-08-06 13:06:07 +0800

    feat(search): 调整搜索引擎排序

--- packages/site/apps/PureSearch/PureSearch.tsx
+++ packages/site/apps/PureSearch/PureSearch.tsx
@@ -27,10 +27,6 @@ interface ISearchEngineer {
 
 const SearchEngineers: ISearchEngineer[] = [
   {
-    name: '百度',
-    url: 'https://www.baidu.com/s?wd=',
-  },
-  {
     name: '必应',
     url: 'https://cn.bing.com/search?q=',
   },
@@ -41,6 +37,10 @@ const SearchEngineers: ISearchEngineer[] = [
   {
     name: 'Google',
     url: 'https://www.google.com/search?q=',
+  },
+  {
+    name: '百度',
+    url: 'https://www.baidu.com/s?wd=',
   },
 ]
 
