2f0c8b3db2833f21a8f62ecdcd760ba7b049256a
EAGzzyCSL <eagzzycsl@hotmail.com>
2023-10-22 20:16:07 +0800

    feat(booklet): 新增《百家姓》

--- packages/site/apps/Booklet/data/index.ts
+++ packages/site/apps/Booklet/data/index.ts
@@ -1,6 +1,6 @@
 import { IBookletItem } from '../type'
 
-export default ['gu_shi_shi_jiu_shou'].map(fileName => {
+export default ['gu_shi_shi_jiu_shou', 'bai_jia_xing'].map(fileName => {
   // eslint-disable-next-line import/no-dynamic-require, @typescript-eslint/no-var-requires, global-require, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
   const markdownModule = require(`./${fileName}.md`).default
   return {
